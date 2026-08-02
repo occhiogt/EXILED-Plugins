acd7b24bc5d53456078d98d0c82ce3e65a7ad012
occhiogt <occhiofly@gmail.com>
2026-08-02 08:00:48 +0200

    Clean up Visual Studio boilerplate comments in AssemblyInfo.cs
    Removed the auto-generated Italian comments from Welcome, scp-018, and
    scp-207; added a real AssemblyDescription for each.

--- scp-018/Properties/AssemblyInfo.cs
+++ scp-018/Properties/AssemblyInfo.cs
@@ -1,33 +1,17 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Le informazioni generali relative a un assembly sono controllate dal seguente 
-// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
-// associate a un assembly.
 [assembly: AssemblyTitle("scp-018")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Increases the duration (in seconds) of SCP-018 before it explodes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("scp-018")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
-// COM, impostare su true l'attributo ComVisible per tale tipo.
 [assembly: ComVisible(false)]
-
-// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
 [assembly: Guid("b1223461-3a46-45ca-8b26-9a28becc3e39")]
 
-// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
-//
-//      Versione principale
-//      Versione secondaria
-//      Numero di build
-//      Revisione
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
